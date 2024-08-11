3ed34bf240dc6e28f6ee97cf0f382445ec352a2a
Geethaka Sankalpa <geethaka.20231659@iit.ac.lk>
2024-08-12 03:16:11 +0530

    Updated header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,6 +4,11 @@ import './Header.css'
 const Header = () => {
   return (
     <div className='header'>
+        <div className="header-content">
+            <h2>Crave it? Click it! Order your favourite food now!</h2>
+            <p>Satisfy your cravings and enjoy delicious meals from the comfort of your home. Order your favourite food now and let us take care of the rest!</p>
+            <button> View Menu </button>
+        </div>
       
     </div>
   )
