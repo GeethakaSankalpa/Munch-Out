cf28290bbb488d54ba6ac8cc0adfb7ae61d2236b
Geethaka Sankalpa <geethaka.20231659@iit.ac.lk>
2024-08-12 00:59:13 +0530

    updated navbar active bar

--- frontend/src/components/NavBar/Navbar.jsx
+++ frontend/src/components/NavBar/Navbar.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
 
 const Navbar = () => {
+    
+    // underline effect on nav elements
+    const[menu, setMenu] = useState("home");
+
+
   return (
     <div className='navbar'>
         <img src={assets.logo} alt='munch out logo' className='logo'/>
         <ul className='navbar-menu'>
-            <li> Home </li>
-            <li> Menu </li>
-            <li> Mobile App </li>
-            <li> Contact Us </li>
+            <li onClick={()=>setMenu("home")} className={menu === "home"?"active":""}> Home </li>
+            <li onClick={()=>setMenu("menu")} className={menu === "menu"?"active":""}> Menu </li>
+            <li onClick={()=>setMenu("mobile")} className={menu === "mobile"?"active":""}> Mobile App </li>
+            <li onClick={()=>setMenu("contact")} className={menu === "contact"?"active":""}> Contact Us </li>
         </ul>
         <div className="navbar-right">
             <img src={assets.search_icon} alt='search'/>
