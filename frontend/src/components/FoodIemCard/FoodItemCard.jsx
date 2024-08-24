781756e4767b8f0449a0d37c02e4b32e8f6717ef
Geethaka Sankalpa <geethaka.20231659@iit.ac.lk>
2024-08-25 03:52:52 +0530

    Added cart item counter display functionality

--- frontend/src/components/FoodIemCard/FoodItemCard.jsx
+++ frontend/src/components/FoodIemCard/FoodItemCard.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './FoodItemCard.css'
 import { assets } from '../../assets/assets'
 
 const FoodItemCard = ({id, name, price, description, image}) => {
+    // inintialize item count of the cart to zero
+    const[itemCount, setItemCount] = useState(0)
+
   return (
     <div className='food-item'>
-        <div className="food-item-container">
-            <img className='food-item-image' src={image} alt=''/>
+        <div className="food-item-image-container">
+            <img className='food-item-image' src={image} alt={name}/>
+            {!itemCount
+                ? <img className='addItem' onClick={()=> setItemCount(prev => prev +1)} src={assets.add_icon_white} alt=""/>
+                : <div className="food-item-counter">
+                    
+                    <img onClick={() => setItemCount(prev => prev + 1)} src={assets.add_icon_green} alt=''/>
+                    <p>{itemCount}</p>
+                    <img onClick={() => setItemCount(prev => prev - 1)} src={assets.remove_icon_red} alt=''/>
+                    
+                </div>
+
+            }
         </div>
 
         <div className="food-item-info">
             <div className="food-item-rating">
                 <p>{name}</p>
-                <img src={assets.rating_starts} alt=''/>
+                <img src={assets.rating_starts} alt='ratings'/>
             </div>
             <p className="food-item-description"> {description} </p>
             <p className="food-item-price"> LKR {price}.00 </p>
