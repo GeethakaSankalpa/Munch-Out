364f303b2be953706a5ea1c1cbcc5bd33fea3fdc
Geethaka Sankalpa <geethaka.20231659@iit.ac.lk>
2024-08-26 01:45:19 +0530

    Added and updated menu items

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -10,6 +10,9 @@ import menu_5 from './menu_5.png'
 import menu_6 from './menu_6.png'
 import menu_7 from './menu_7.png'
 import menu_8 from './menu_8.png'
+import menu_9 from './menu_9.png'
+import menu_10 from './menu_10.png'
+import menu_11 from './menu_11.png'
 
 import food_1 from './food_1.png'
 import food_2 from './food_2.png'
@@ -114,6 +117,18 @@ export const menu_list = [
     {
         menu_name: "Noodles",
         menu_image: menu_8
+    },
+    {  
+        menu_name: "Burgers",
+        menu_image:menu_9
+    },
+    {  
+        menu_name: "Pizza",
+        menu_image:menu_10
+    },
+    {  
+        menu_name: "Beverages",
+        menu_image:menu_11
     }]
 
 export const food_list = [
