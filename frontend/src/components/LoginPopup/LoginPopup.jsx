e5b184d2c8cf1bb0bce5a150adaed940f0dfa0cd
Geethaka Sankalpa <geethaka.20231659@iit.ac.lk>
2024-08-25 21:17:37 +0530

    Added login form

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './LoginPopup.css'
+import { assets } from '../../assets/assets'
 
-const LoginPopup = () => {
+const LoginPopup = ({setShowLogin}) => {
+
+    const[currentState, setCurrentState] = useState("Log In")
+
   return (
     <div className='login-popup'>
-        login
+        <form className="login-popup-container">
+            <div className="login-popup-title">
+                <h2> {currentState} </h2>
+                <img onClick={() => setShowLogin(false)} src={assets.cross_icon} alt=''/>
+            </div>
+
+            <div className="login-popup-inputs">
+                {currentState === "Login"?<></>: <input type='text' placeholder='Your Name' required/>}
+                <input type='email' placeholder='Your Email' required/>
+                <input type='password' placeholder='Password' required/>
+            </div>
+            <button> {currentState === "Sign Up"?"Create account":"Login"} </button>
+            <div className="login-popup-condition">
+                <input type="checkbox" required/>
+                <p> By continuing, I agree to the terms of use & privacy policy. </p>
+            </div>
+        </form>
       
     </div>
   )
