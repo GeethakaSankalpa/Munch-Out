54e233fb699bd4c47f3bcfe00465cbb8117f99f4
Geethaka Sankalpa <geethaka.20231659@iit.ac.lk>
2024-08-27 22:16:50 +0530

    Design cart page

--- frontend/src/components/NavBar/Navbar.jsx
+++ frontend/src/components/NavBar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = ({setShowLogin}) => {
 
   return (
     <div className='navbar'>
-        <Link to={'./'}><img src={assets.logo} alt='munch out logo' className='logo'/></Link>
+        <Link to={'/'}><img src={assets.logo} alt='munch out logo' className='logo'/></Link>
         <ul className='navbar-menu'>
             <Link to='/' onClick={()=>setMenu("home")} className={menu === "home"?"active":""}> Home </Link>
             <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu === "menu"?"active":""}> Menu </a>
@@ -21,7 +21,7 @@ const Navbar = ({setShowLogin}) => {
         <div className="navbar-right">
             <img src={assets.search_icon} alt='search'/>
             <div className="navbar-search-icon">
-                <Link to='./cart'><img src={assets.basket_icon} alt='basket'/></Link>
+                <Link to='/Cart'><img src={assets.basket_icon} alt='basket'/></Link>
                 <div className="cart-add-dot"></div>
             </div>
             <button onClick={() => setShowLogin (true)}> Sign In </button>
--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -5,30 +5,69 @@ import { StoreFoodContext } from '../../FoodContext/StoreFoodContext'
 const Cart = () => {
 
   // remove from cart functionality
-  const { cartItems, foodList, removeFromCart } = useContext(StoreFoodContext);
+  const { cartItems, food_list, removeFromCart } = useContext(StoreFoodContext);
 
   return (
     <div className='cart'>
       <div className="cart-items">
         <div className="cart-items-title">
-          <p> Items </p>
-          <p> Title </p>
-          <p> Price </p>
-          <p> Quantity </p>
-          <p> Total </p>
-          <p> Remove </p>
+          <h2><p> Items </p></h2>
+          <h2><p> Title </p></h2>
+          <h2><p> Price </p></h2>
+          <h2><p> Quantity </p></h2>
+          <h2><p> Total </p></h2>
+          <h2><p> Remove </p></h2>
         </div>
         <br />
         <hr />
-        {foodList.map((item, index) => {
+        {food_list.map((item, index) => {
           if (cartItems[item._id] > 0) {
             return (
-              <div className="cart-items-title cart-items-item">
-                <p> {item.name} </p>
+              <div>
+                <div className="cart-items-title cart-items-item">
+                  <img src={item.image} alt='' />
+                  <p> {item.name} </p>
+                  <p> LKR {item.price} </p>
+                  <p> {cartItems[item._id]} </p>
+                  <p> LKR {item.price * cartItems[item._id]} </p>
+                  <p onClick={() => removeFromCart(item._id)} className='remove-cross'> x </p>
+                </div>
+                <hr />
               </div>
             )
           }
         })}
+        <div className="cart-bottom">
+          <div className="cart-total">
+            <h2> Cart Totals </h2>
+            <div>
+              <div className='cart-total-details'>
+                <p> Sub Total </p>
+                <p> {0} </p>
+              </div>
+              <hr/>
+              <div className='cart-total-details'>
+                <p> Delivery Fee </p>
+                <p> {2} </p>
+              </div>
+              <hr/>
+              <div className='cart-total-details'>
+                <b> Total </b>
+                <b> {0} </b>
+              </div>  
+            </div>
+            <button> PROCEED TO CHECKOUT </button>
+          </div>
+          <div className="cart-promo-code">
+            <div>
+              <p> If you have a promo code, Enter it here </p>
+              <div className="cart-promo-code-input">
+                <input type="text" placeholder='Promo Code' />
+                <button> Submit </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
