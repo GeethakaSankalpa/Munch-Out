0bdff73ec32ee164d02d6a9cb0f397653d538d17
Geethaka Sankalpa <geethaka.20231659@iit.ac.lk>
2024-09-02 12:20:58 +0530

    Create Backend of Munch Out

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express"
+import cors from "cors"
+
+// app config
+const app = express()
+const port = 4000
+
+// middleware = will get the request from frontend to backend
+app.use(express.json())
+app.use(cors()) // access the backend from any frontend
+
+/**
+ * HTTP method that can request data from the server
+ * Whenever we hit any URL in the browser, that will be executed as a get method */ 
+app.get("/", (request, response) => {
+    response.send("API Working")
+})   
+
+// run express server
+app.listen(port, () => {
+    console.log(`Server Started on http://localhost:${port}`)
+})
