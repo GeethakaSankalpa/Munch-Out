2ff9a81bc10cb16a9f04fe82878dea1a95f15088
Geethaka Sankalpa <sankalpa2003geethaka03@gmail.com>
2024-09-28 00:56:14 +0530

    Updated footer app description text

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -3,46 +3,48 @@ import './Footer.css'
 import { assets } from '../../assets/assets'
 
 const Footer = () => {
-  return (
-    <div className='footer' id='footer'>
-        <div className="footer-content">
-            <div className="footer-content-left">
-                <img className='footer-logo' src={assets.logo} alt=''/>
-                <p> One dummy text </p>
+    return (
+        <div className='footer' id='footer'>
+            <div className="footer-content">
+                <div className="footer-content-left">
+                    <img className='footer-logo' src={assets.logo} alt='' />
+                    <p> Munch Out is your go-to food delivery app, bringing delicious meals from local restaurants right to your doorstep. Enjoy a variety of cuisines with quick and easy ordering, exclusive deals, and fast delivery—all from the comfort of your home. Join us today and elevate your food experience!
 
-                <div className="footer-social-icons">
-                    <img src={assets.facebook_icon} alt='' />
-                    <img src={assets.twitter_icon} alt='' />
-                    <img src={assets.linkedin_icon} alt='' />
+                    </p>
+
+                    <div className="footer-social-icons">
+                        <img src={assets.facebook_icon} alt='' />
+                        <img src={assets.twitter_icon} alt='' />
+                        <img src={assets.linkedin_icon} alt='' />
+                    </div>
+                </div>
+
+                <div className="footer-content-center">
+                    <h2> COMPANY </h2>
+                    <ul>
+                        <li> Home </li>
+                        <li> About Us </li>
+                        <li> Delivery </li>
+                        <li> Privacy Policy </li>
+                    </ul>
+
+                </div>
+
+                <div className="footer-content-right">
+                    <h2> GET IN TOUCH </h2>
+                    <ul>
+                        <li> 0112 455 978 </li>
+                        <li> contact@munchout.com </li>
+                    </ul>
                 </div>
             </div>
+            <hr />
+            <p className="footer-copyright">
+                Copyright 2024 @ Munchout.com - All Rights Reserved.
+            </p>
 
-            <div className="footer-content-center">
-                <h2> COMPANY </h2>
-                <ul>
-                    <li> Home </li>
-                    <li> About Us </li>
-                    <li> Delivery </li>
-                    <li> Privacy Policy </li>
-                </ul>
-                
-            </div>
-            
-            <div className="footer-content-right">
-                <h2> GET IN TOUCH </h2>
-                <ul>
-                    <li> 0112 455 978 </li>
-                    <li> contact@munchout.com </li>
-                </ul>
-            </div>
         </div>
-        <hr/>
-        <p className="footer-copyright">
-            Copyright 2024 @ Munchout.com - All Rights Reserved.
-        </p>
-      
-    </div>
-  )
+    )
 }
 
 export default Footer
